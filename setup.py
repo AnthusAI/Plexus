376aa12d2ae5f54f2d1945539429e5d8cff5c6f7
Ryan Alyn Porter <rap@endymion.com>
2024-04-11 14:47:43 -0400

    Fixed setup.

--- setup.py
+++ setup.py
@@ -5,21 +5,21 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        python-dotenv==1.0.0
-        typer[all]==0.9.0
-        pandas==2.1.4
-        openai==1.6.1
-        tenacity==8.2.3
-        nltk
-        pybind11
-        fasttext
-        tiktoken
-        seaborn
-        litellm
-        boto3
-        git+https://github.com/Anth-us/openai_cost_calculator.git@main
-        graphviz
-        mistune
-        pyyaml
+        'python-dotenv==1.0.0'
+        'typer[all]==0.9.0'
+        'pandas==2.1.4'
+        'openai==1.6.1'
+        'tenacity==8.2.3'
+        'nltk'
+        'pybind11'
+        'fasttext'
+        'tiktoken'
+        'seaborn'
+        'litellm'
+        'boto3'
+        'git+https://github.com/Anth-us/openai_cost_calculator.git@main'
+        'graphviz'
+        'mistune'
+        'pyyaml'
     ],
 )
