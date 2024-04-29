0147a9fce26e9b358291ebc2fc8c5bb28d0d64b9
Ryan Alyn Porter <rap@endymion.com>
2024-04-29 19:03:04 -0400

    Make `python -m plexus` work.

---
+++ plexus/__main__.py
@@ -0,0 +1,4 @@
+from plexus.cli.CommandLineInterface import main
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     package_data={
-        'plexus': ['templates/*'],
+        'plexus': ['templates/*', '__main__.py'],
     },
     entry_points={
         'console_scripts': [
