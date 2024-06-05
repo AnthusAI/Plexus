f0a473746887b532a0fd942c59f08cb8e987f4eb
Ryan Alyn Porter <rap@endymion.com>
2024-06-05 17:12:55 +0000

    Data processors now process dataframes.
    Before, the processor was invoked one time per transcript.  Now, it has more control over the whole dataframe, so it can remove or add new labeled samples.

--- plexus/processors/RemoveStopWordsTranscriptFilter.py
+++ plexus/processors/RemoveStopWordsTranscriptFilter.py
@@ -1,14 +1,18 @@
+import pandas as pd
 import nltk
 from nltk.corpus import stopwords
-from plexus.processors.TranscriptFilter import TranscriptFilter
+from plexus.processors.DataframeProcessor import DataframeProcessor
 
-class RemoveStopWordsTranscriptFilter(TranscriptFilter):
+class RemoveStopWordsTranscriptFilter(DataframeProcessor):
 
     def __init__(self):
         nltk.download('stopwords')
         self.stop_words = set(stopwords.words('english'))
 
-    def process(self, *, transcript):
-        words = transcript.split()
-        filtered_words = [word for word in words if word.lower() not in self.stop_words]
-        return ' '.join(filtered_words)
+    def process(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        dataframe["Transcription"] = dataframe["Transcription"].apply(
+            lambda transcript: ' '.join(
+                [word for word in transcript.split() if word.lower() not in self.stop_words]
+            )
+        )
+        return dataframe
