1158d52c0d9799b805c1a0511aed6a745f56269f
Ryan Alyn Porter <rap@endymion.com>
2024-07-26 01:25:49 +0000

    Safeguard to check if the entity is really there.

--- plexus/scores/AgenticExtractor.py
+++ plexus/scores/AgenticExtractor.py
@@ -3,15 +3,22 @@ from plexus.CustomLogging import logging
 from plexus.scores.LangGraphScore import LangGraphScore
 from typing import Dict, TypedDict, Optional, Literal, Annotated
 from langchain.output_parsers import ResponseSchema, StructuredOutputParser
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
+from langchain_core.messages import HumanMessage, AIMessage
 from langchain_openai import ChatOpenAI
 from langgraph.graph import StateGraph, END
-from langgraph.graph.message import add_messages
+from langgraph.graph.message import add_messages, AnyMessage
+
+# Logging.
+from langchain.globals import set_debug
+set_debug(True)
 
 class ExtractorState(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
     transcript: str
     entity: Optional[str]
     quote: Optional[str]
+    validation_error: Optional[str]
     
 class AgenticExtractor(LangGraphScore):
     
@@ -21,10 +28,6 @@ class AgenticExtractor(LangGraphScore):
 
     def __init__(self, scorecard_name, score_name, **kwargs):
         super().__init__(scorecard_name=scorecard_name, score_name=score_name, **kwargs)
-        
-        # Logging.
-        from langchain.globals import set_debug
-        set_debug(True)
 
     def load_context(self, context):
         pass
@@ -42,49 +45,69 @@ class AgenticExtractor(LangGraphScore):
             ]
             output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
 
-            format_instructions = output_parser.get_format_instructions()
-            prompt = PromptTemplate(
-                template="""
+
+            template_string = """
 {format_instructions}
 
 This is the transcript of a call center phone call that we're reviewing for QA purposes:
 {transcript}
 
 {prompt}
-""",
-                input_variables=["transcript", "prompt"],
-                partial_variables={"format_instructions": format_instructions},
+"""
+            prompt = PromptTemplate(
+                template=template_string,
+                input_variables=["transcript", "prompt"]
             )
+            
+            if state["validation_error"] is not None:
+                messages = [
+                    template_string,
+                    state['messages'][-1],
+                    HumanMessage(content=state["validation_error"])
+                ]
+                prompt = ChatPromptTemplate.from_messages(
+                    messages=messages
+                )
 
             model = ChatOpenAI(temperature=0)
-            chain = prompt | model | output_parser
-            
-            result = chain.invoke({
+            chain = prompt | model
+        
+            output = chain.invoke({
                 "transcript": model_input.transcript, 
-                "prompt": self.parameters.prompt
+                "prompt": self.parameters.prompt,
+                "format_instructions": output_parser.get_format_instructions()
             })
+            
+            result = output_parser.invoke(output)
 
             return {
-                "entity": result["entity"],
-                "quote": result["quote"]
+                "messages": output,
+                "entity":   result["entity"],
+                "quote":    result["quote"]
             }
+        
+        def _verify_entity_node(state: ExtractorState) -> Dict[str, str]:
+            entity = state.get("entity", "")
+            transcript = state.get("transcript", "")
 
-        def clean_quote(quote: str) -> str:
-            quote = ' '.join(quote.split())  # Condense any whitespace into single characters
-            if not (quote.startswith('“') and quote.endswith('”')):
-                if quote.startswith("'") and quote.endswith("'"):
-                    quote = f'“{quote[1:-1]}”'
-                elif not (quote.startswith('"') and quote.endswith('"')):
-                    quote = f'“{quote}”'
-            return quote
+            if entity and entity not in transcript:
+                return {"validation_error": f"No, that's not possible: The string \"{entity}\" does not exist within the transcript."}
+            else:
+                return {"validation_error": None}
+
+        def _next_after_verify_entity(state: ExtractorState) -> Dict[str, str]:
+            if state["validation_error"] is not None:
+                return "extract_entity"
+            return END
 
         workflow = StateGraph(ExtractorState)
 
         workflow.add_node("extract_entity", _extract_entity_node)
-        
         workflow.set_entry_point("extract_entity")
-
-        workflow.add_edge("extract_entity", END)
+        workflow.add_edge("extract_entity", "verify_entity")
+        
+        workflow.add_node("verify_entity", _verify_entity_node)
+        workflow.add_conditional_edges("verify_entity", _next_after_verify_entity)
 
         app = workflow.compile()
 
@@ -95,6 +118,16 @@ This is the transcript of a call center phone call that we're reviewing for QA p
             LangGraphScore.ModelOutput(
                 score_name =  self.parameters.score_name,
                 score =       result["entity"],
-                explanation = clean_quote(result["quote"])
+                explanation = AgenticExtractor.clean_quote(result["quote"])
             )
         ]
+
+    @staticmethod
+    def clean_quote(quote: str) -> str:
+        quote = ' '.join(quote.split())  # Condense any whitespace into single characters
+        if not (quote.startswith('“') and quote.endswith('”')):
+            if quote.startswith("'") and quote.endswith("'"):
+                quote = f'“{quote[1:-1]}”'
+            elif not (quote.startswith('"') and quote.endswith('"')):
+                quote = f'“{quote}”'
+        return quote
