12a44aa8458fd15c5686242480dbcf234078b93b
Derek Norrbom <dereknorrbom@gmail.com>
2024-08-08 18:57:11 +0000

    MultiClassClassifier

--- plexus/scores/nodes/__init__.py
+++ plexus/scores/nodes/__init__.py
@@ -2,3 +2,4 @@ from .BaseNode import BaseNode
 from .BeforeAfterSlicer import BeforeAfterSlicer
 from .YesOrNoClassifier import YesOrNoClassifier
 from .AgenticExtractor import AgenticExtractor
+from .MultiClassClassifier import MultiClassClassifier
