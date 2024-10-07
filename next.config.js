0ce32dad3e63e973fafaadc8caca505adf195658
Ryan Alyn Porter <rap@endymion.com>
2024-10-06 20:18:59 -0400

    Build fixes.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ['@aws-crypto'],
+    }
+}
 
 module.exports = nextConfig
