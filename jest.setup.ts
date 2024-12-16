521afe5dbbc66ec9d416dda3b28491dac630c019
Ryan Alyn Porter <rap@endymion.com>
2024-12-16 14:32:16 -0500

    Fixes for the testing setup.

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,11 +1,1 @@
 import '@testing-library/jest-dom'
-import type { expect } from '@jest/globals'
-
-declare module '@jest/globals' {
-  interface Matchers<R extends void | Promise<void>> {
-    toBeInTheDocument(): R
-    toHaveLength(length: number): R
-    toHaveClass(...classNames: string[]): R
-    toHaveAttribute(attr: string, value?: string): R
-  }
-}
