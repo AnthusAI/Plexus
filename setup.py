4cbc2dfb50d46743e41cc8c24efb7e071472fef6
Ryan Alyn Porter <rap@endymion.com>
2024-12-30 18:09:41 -0500

    Final testing of release DevOps.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='plexus',
-    version='0.1.0',
+    version='0.0.0',
     packages=find_packages(),
     package_data={
         'plexus': ['templates/*', '__main__.py'],
