24dd61fb23b4b284d11f74dcb541e93eb1ad85be
Ryan Alyn Porter <rap@endymion.com>
2024-12-30 14:36:32 -0500

    Remove AI cruft.

--- src/types/aws-amplify.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'aws-amplify' {
-  export const API: {
-    graphql: (operation: any) => Promise<any>;
-  };
-  export const graphqlOperation: (query: string, variables?: any) => any;
-} 
