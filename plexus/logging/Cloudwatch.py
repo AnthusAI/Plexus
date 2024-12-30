f065d118ad94103b2b0d39b48675353679a1dc5b
Ryan Alyn Porter <rap@endymion.com>
2024-12-30 17:42:27 -0500

    CI fixes.

--- plexus/logging/Cloudwatch.py
+++ plexus/logging/Cloudwatch.py
@@ -1,11 +1,15 @@
 import boto3
 import logging
 from botocore.exceptions import ClientError
+import os
 
 class CloudWatchLogger:
     def __init__(self, namespace="Plexus"):
-        self.cloudwatch_client = boto3.client('cloudwatch')
         self.namespace = namespace
+        self.cloudwatch_client = None
+        if os.getenv('AWS_ACCESS_KEY_ID') and os.getenv('AWS_SECRET_ACCESS_KEY') \
+                and os.getenv('AWS_DEFAULT_REGION'):
+            self.cloudwatch_client = boto3.client('cloudwatch')
 
     def log_metric(self, metric_name, metric_value, dimensions):
         """
@@ -16,6 +20,10 @@ class CloudWatchLogger:
             metric_value (float): Value of the metric
             dimensions (dict): Dictionary of dimension names and values
         """
+        if not self.cloudwatch_client:
+            logging.debug(f"CloudWatch not configured, skipping metric: {metric_name}")
+            return
+
         try:
             metric_data = {
                 'MetricName': metric_name,
