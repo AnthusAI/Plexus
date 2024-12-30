1aad8d0ccf1e920713c581ccaf951e93229a1664
Ryan Alyn Porter <rap@endymion.com>
2024-12-30 15:05:26 -0500

    Disable Tensorflow stuff for now, to get stable CI working.

--- plexus/scores/__init__.py
+++ plexus/scores/__init__.py
@@ -4,12 +4,12 @@ It includes various classifiers such as machine learning classifiers, deep learn
 and explainable classifiers.  These are the score classes that are referenced in the scorecard YAML files.
 """
 from plexus.scores.Score import Score
-from plexus.scores.CompositeScore import CompositeScore
-from plexus.scores.DeepLearningSemanticClassifier import DeepLearningSemanticClassifier
-from plexus.scores.DeepLearningOneStepSemanticClassifier import DeepLearningOneStepSemanticClassifier
-from plexus.scores.DeepLearningSlidingWindowSemanticClassifier import DeepLearningSlidingWindowSemanticClassifier
-from plexus.scores.FastTextClassifier import FastTextClassifier
-from plexus.scores.ExplainableClassifier import ExplainableClassifier
+# from plexus.scores.CompositeScore import CompositeScore
+# from plexus.scores.DeepLearningSemanticClassifier import DeepLearningSemanticClassifier
+# from plexus.scores.DeepLearningOneStepSemanticClassifier import DeepLearningOneStepSemanticClassifier
+# from plexus.scores.DeepLearningSlidingWindowSemanticClassifier import DeepLearningSlidingWindowSemanticClassifier
+# from plexus.scores.FastTextClassifier import FastTextClassifier
+# from plexus.scores.ExplainableClassifier import ExplainableClassifier
 from plexus.scores.AgenticExtractor import AgenticExtractor
 from plexus.scores.AgenticValidator import AgenticValidator
 from plexus.scores.AWSComprehendEntityExtractor import AWSComprehendEntityExtractor
