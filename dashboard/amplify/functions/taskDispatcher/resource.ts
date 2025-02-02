80f5afd4e2ea06bd97c53cc85d720cd8f2241154
Ryan Alyn Porter <rap@endymion.com>
2025-02-02 20:02:10 +0000

    Ported task dispatch Lambda to Python, to use Celery directly.

---
+++ dashboard/amplify/functions/taskDispatcher/resource.ts
@@ -0,0 +1,30 @@
+import { CfnOutput, Stack, StackProps, Duration } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
+
+export class TaskDispatcherStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // Define the Lambda function
+    const taskDispatcherFunction = new lambda.Function(this, 'TaskDispatcherFunction', {
+      runtime: lambda.Runtime.PYTHON_3_11,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname)),
+      functionName: 'TaskDispatcherFunction',
+      description: 'Dispatches tasks from DynamoDB to Celery',
+      timeout: Duration.seconds(30),
+      environment: {
+        CELERY_BROKER_URL: process.env.CELERY_BROKER_URL || '',
+        CELERY_RESULT_BACKEND: process.env.CELERY_RESULT_BACKEND || ''
+      }
+    });
+
+    // Output the Lambda function ARN
+    new CfnOutput(this, 'TaskDispatcherFunctionArn', {
+      value: taskDispatcherFunction.functionArn,
+      exportName: 'TaskDispatcherFunctionArn'
+    });
+  }
+} 
