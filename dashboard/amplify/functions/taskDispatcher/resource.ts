fd11a5844273f7463076e303407680c1906e2fa7
Ryan Alyn Porter <rap@endymion.com>
2025-02-02 21:32:13 +0000

    Aligning function with Gen2.

--- dashboard/amplify/functions/taskDispatcher/resource.ts
+++ dashboard/amplify/functions/taskDispatcher/resource.ts
@@ -4,7 +4,7 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const functionDir = path.dirname(fileURLToPath(import.meta.url));
 
 export class TaskDispatcherStack extends Stack {
   public readonly function: lambda.Function;
@@ -16,9 +16,15 @@ export class TaskDispatcherStack extends Stack {
     this.function = new lambda.Function(this, 'TaskDispatcherFunction', {
       runtime: lambda.Runtime.PYTHON_3_11,
       handler: 'index.handler',
-      code: lambda.Code.fromAsset(__dirname),
-      functionName: 'TaskDispatcherFunction',
-      description: 'Dispatches tasks from DynamoDB to Celery',
+      code: lambda.Code.fromAsset(functionDir, {
+        bundling: {
+          image: lambda.Runtime.PYTHON_3_11.bundlingImage,
+          command: [
+            'bash', '-c',
+            'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output'
+          ]
+        }
+      }),
       timeout: Duration.seconds(30),
       environment: {
         CELERY_BROKER_URL: process.env.CELERY_BROKER_URL || '',
