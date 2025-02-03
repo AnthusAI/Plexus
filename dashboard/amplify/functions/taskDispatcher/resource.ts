236ef1560158a964a22bf36cd836436d12d45c98
Ryan Alyn Porter <rap@endymion.com>
2025-02-03 13:20:39 +0000

    Refactor TaskDispatcher stack to pass Task table as a prop

--- dashboard/amplify/functions/taskDispatcher/resource.ts
+++ dashboard/amplify/functions/taskDispatcher/resource.ts
@@ -5,16 +5,25 @@ import * as path from 'path';
 import { fileURLToPath } from 'url';
 import { createRequire } from 'module';
 import { execSync } from 'child_process';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { DynamoEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import { StartingPosition } from 'aws-cdk-lib/aws-lambda';
+import { Policy, PolicyStatement, Effect } from 'aws-cdk-lib/aws-iam';
 
 // Get the directory path in ES module context
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Interface for TaskDispatcher stack props
+interface TaskDispatcherStackProps extends StackProps {
+  taskTable: dynamodb.Table;
+}
+
 // Custom CDK stack for the Python Task Dispatcher function
 export class TaskDispatcherStack extends Stack {
   public readonly taskDispatcherFunction: lambda.Function;
 
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: TaskDispatcherStackProps) {
     super(scope, id, props);
     
     // Get the directory containing the function code
@@ -56,6 +65,44 @@ export class TaskDispatcherStack extends Stack {
         CELERY_RESULT_BACKEND: process.env.CELERY_RESULT_BACKEND || ''
       }
     });
+
+    // Enable streams on the Task table using L1 construct
+    const cfnTable = props.taskTable.node.defaultChild as dynamodb.CfnTable;
+    if (cfnTable) {
+      cfnTable.streamSpecification = {
+        streamViewType: dynamodb.StreamViewType.NEW_AND_OLD_IMAGES
+      };
+    }
+
+    // Create stream policy
+    const policy = new Policy(this, 'TaskDispatcherStreamPolicy', {
+      statements: [
+        new PolicyStatement({
+          effect: Effect.ALLOW,
+          actions: [
+            'dynamodb:DescribeStream',
+            'dynamodb:GetRecords',
+            'dynamodb:GetShardIterator',
+            'dynamodb:ListStreams'
+          ],
+          resources: [props.taskTable.tableArn + '/stream/*']
+        })
+      ]
+    });
+
+    // Attach the policy to the Lambda function
+    this.taskDispatcherFunction.role?.attachInlinePolicy(policy);
+
+    // Create event source mapping
+    const eventSource = new DynamoEventSource(props.taskTable, {
+      startingPosition: StartingPosition.LATEST,
+      batchSize: 1,
+      retryAttempts: 3,
+      enabled: true
+    });
+
+    // Add the event source to the Lambda function
+    this.taskDispatcherFunction.addEventSource(eventSource);
 
     new CfnOutput(this, 'TaskDispatcherFunctionArn', {
       value: this.taskDispatcherFunction.functionArn,
