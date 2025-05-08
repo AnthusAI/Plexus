8d0c9db3a10a509916447be54fdb3154af1f1006
Ryan Alyn Porter <rap@endymion.com>
2025-05-08 14:52:22 +0000

    This complexity is not justified.  We'll simply store this information in the configuration.

--- dashboard/amplify/data/resource.ts
+++ dashboard/amplify/data/resource.ts
@@ -164,7 +164,6 @@ const schema = a.schema({
             createdAt: a.datetime().required(),
             updatedAt: a.datetime().required(),
             note: a.string(),
-            validClasses: a.json(),
             scoreResults: a.hasMany('ScoreResult', 'scoreVersionId'),
             scoresAsChampion: a.hasMany('Score', 'championVersionId'),
             parentVersionId: a.string(),
