f2dd67dab507d09b1e4cf0012d5560de698cd6ff
dereknorrbom <dereknorrbom@gmail.com>
2025-10-15 21:10:13 +0000

    feat(resource): Add new secondary index for type with sorting by status and updatedAt
    - Introduced a new secondary index on the resource schema to enable efficient querying by type, with results sorted by status and updatedAt.

--- dashboard/amplify/data/resource.ts
+++ dashboard/amplify/data/resource.ts
@@ -453,6 +453,7 @@ const schema = a.schema({
             index("scoreId"),
             index("scorecardId").sortKeys(["scoreId", "itemId"]).name("byScorecardScoreItem"),
             index("itemId").sortKeys(["scorecardId", "scoreId"]).name("byItemScorecardScore"),
+            index("type").sortKeys(["status", "updatedAt"]).name("byTypeStatusUpdated"),
         ]),
 
     BatchJobScoringJob: a
