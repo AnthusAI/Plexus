a6a126d3743b2342f5de6cfe50b38629adb74d66
Ryan Alyn Porter <rap@endymion.com>
2026-04-27 12:31:04 -0400

    Make metrics aggregation config testable without CDK

--- infrastructure/stacks/metrics_aggregation_stack.py
+++ infrastructure/stacks/metrics_aggregation_stack.py
@@ -20,17 +20,7 @@ import os
 
 from .shared.naming import get_resource_name
 from .shared.config import EnvironmentConfig
-
-
-METRICS_TABLE_TYPES = ['item', 'scoreresult', 'task', 'evaluation', 'procedure']
-
-METRICS_STREAM_CONFIGS = {
-    'item': {'batch_size': 10, 'batch_window': 15},
-    'scoreresult': {'batch_size': 100, 'batch_window': 15},
-    'task': {'batch_size': 1, 'batch_window': 15},
-    'evaluation': {'batch_size': 1, 'batch_window': 15},
-    'procedure': {'batch_size': 1, 'batch_window': 15},
-}
+from .metrics_aggregation_config import METRICS_STREAM_CONFIGS, METRICS_TABLE_TYPES
 
 
 class MetricsAggregationStack(Stack):
